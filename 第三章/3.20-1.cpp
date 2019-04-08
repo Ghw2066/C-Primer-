5234734488a60b986c2d1ec38d27fd53be35895d
Haowen <guohaowen1995@outlook.com>
2019-04-08 14:05:23 +0800

    3.20-1.cpp

---
+++ 第三章/3.20-1.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int i;
+	vector<int> ivec;
+	while (cin >> i) {
+		ivec.push_back(i);
+	}
+	auto j = ivec.size();
+	for (decltype(ivec.size()) j = 0; j != ivec.size(); j++) {
+		if (j > 0) {
+			cout << (ivec[j] + ivec[j - 1]) << ' ';
+		}
+	}
+
+	return 0;
+}
